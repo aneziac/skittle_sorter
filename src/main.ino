d1b0cb9040d1f2d9dd5f20812e872e8939b91ac0
aneziac <nateannau@gmail.com>
2021-10-05 23:16:11 -0700

    Use actual correct datatype

--- src/main.ino
+++ src/main.ino
@@ -19,13 +19,13 @@ void setup() {
 
 void loop() {
   uint16_t clear, red, green, blue;
-  uint8_t n_red, n_green, n_blue;
+  uint16_t n_red, n_green, n_blue;
 
   tcs.getRawData(&red, &green, &blue, &clear);
 
-  n_red = int(red / red_max);
-  n_green = int(green / green_max);
-  n_blue = int(blue / blue_max);
+  n_red = int(255.0 * red / red_max);
+  n_green = int(255.0 * green / green_max);
+  n_blue = int(255.0 * blue / blue_max);
 
   Serial.print("R\t"); Serial.print(n_red);
   Serial.print("\tG\t"); Serial.print(n_green);
