cc38ae85443c029a7c135477e4eb7858cc4b5c00
aneziac <nateannau@gmail.com>
2021-11-07 12:42:17 -0800

    Structs for servo angles

--- src/main/main.ino
+++ src/main/main.ino
@@ -30,8 +30,12 @@ const Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_540MS, 
 const uint16_t red_max = 546, green_max = 816, blue_max = 536;
 const double scaled_max = 255.0;
 
-const uint16_t drop_angle = 180, sense_angle = 94, sort_angle = 30;
-const uint16_t purple_angle = 0, yellow_angle = 0, red_angle = 0, blue_angle = 0, green_angle = 0;
+struct {  // top servo angles
+  const uint16_t drop = 180, sense = 94, sort = 30;
+} top_servo_angle;
+struct {
+  const uint16_t purple = 0, yellow = 0, red = 0, blue = 0, green = 0;
+} bottom_servo_angle;
 
 bool calibrating = true;
 
@@ -58,7 +62,7 @@ void setup() {
     while (1);
   }
 
-  top_servo.write(drop_angle); // Return to default state
+  top_servo.write(top_servo_angle.drop); // Return to default state
   delay(2000);
 }
 
@@ -66,8 +70,8 @@ void loop() {
   uint16_t n_red, n_green, n_blue;
 
   if (calibrating) {
-    for (int i = 0; i < 10; i++) {
-      top_servo.write(sense_angle - 5 + i);
+    for (int i = 0; i < 7; i++) {
+      top_servo.write(top_servo_angle.sense - 3 + i);
       get_norm_rgb(n_red, n_green, n_blue);
 
       Serial.print("R\t"); Serial.print(n_red);
@@ -76,38 +80,38 @@ void loop() {
       Serial.println();
 
       delay(1000);
-      top_servo.write(drop_angle);
+      top_servo.write(top_servo_angle.drop);
     }
 
-    top_servo.write(drop_angle);
+    top_servo.write(top_servo_angle.drop);
     return 0;
   }
 
-  top_servo.write(sense_angle)
+  top_servo.write(top_servo_angle.sense);
   get_norm_rgb(n_red, n_green, n_blue);
 
   /*
   if (n_blue > 265 && n_green > 195) {
     Serial.print("Skittle is purple\n");
-    bottom_servo.write(purple_angle);
+    bottom_servo.write(bottom_servo_angle.purple);
   } else if (n_red > 220 && n_green > 180) {
     Serial.print("Skittle is yellow\n");
-    bottom_servo.write(yellow_angle);
+    bottom_servo.write(bottom_servo_angle.yellow);
   } else if (n_red > 210) {
     Serial.print("Skittle is orange\n");
-    bottom_servo.write(orange_angle);
+    bottom_servo.write(bottom_servo_angle.orange);
   } else if (n_green > 175 && n_blue > 240) {
     Serial.print("Skittle is green\n");
-    bottom_servo.write(green_angle);
+    bottom_servo.write(bottom_servo_angle.green);
   } else {
     Serial.print("Skittle is red\n");
-    bottom_servo.write(red_angle);
+    bottom_servo.write(bottom_servo_angle.red);
   }
   */
 
   delay(2000);
-  top_servo.write(sort_angle);
+  top_servo.write(top_servo_angle.sort);
   delay(1000);
-  top_servo.write(drop_angle);
+  top_servo.write(top_servo_angle.drop);
   delay(5000);
 }
